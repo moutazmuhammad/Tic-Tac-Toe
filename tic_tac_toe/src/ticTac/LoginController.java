09ccff35d29fe217c95a8ff963c764de4870b8ba
ZainabSabry <83887860+ZainabSabry@users.noreply.github.com>
2022-02-16 00:40:50 +0200

    Signup and login earlier validation

--- tic_tac_toe/src/ticTac/LoginController.java
+++ tic_tac_toe/src/ticTac/LoginController.java
@@ -4,12 +4,14 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 /**
  *
@@ -17,18 +19,46 @@ import javafx.scene.control.TextField;
  */
 public class LoginController implements Initializable {
     
-    
+    @FXML
     private TextField username;
+    @FXML
     private TextField password;
+    @FXML
     private Hyperlink signUpHyperlink;
+    @FXML
     private Button loginButton;
+    @FXML
     private Label usernameValid;
+    @FXML
     private Label passwordValid;
+    @FXML
+    private Label passwdAndUser;
     
     @FXML
     private void loginButtonAction(ActionEvent event) throws IOException{
-         MainScreen mainScreen = new MainScreen();
-         mainScreen.changeScene("mainMenu.fxml");
+         //Make sure the user doesn't leave the username or password empty
+        if(username.getText().isEmpty() || password.getText().isEmpty()){    
+            passwdAndUser.setVisible(true);
+            //Once he/she starts writing, invalid label disappears
+            username.addEventHandler(KeyEvent.KEY_TYPED, new EventHandler<KeyEvent>(){
+                @Override
+                public void handle(KeyEvent event) {
+                    passwdAndUser.setVisible(false);
+                }
+        });
+            //Same with password
+            password.addEventHandler(KeyEvent.KEY_TYPED, new EventHandler<KeyEvent>(){
+                @Override
+                public void handle(KeyEvent event) {
+                    passwdAndUser.setVisible(false);
+                }
+                
+            });
+        }else{
+            MainScreen mainScreen = new MainScreen();
+            mainScreen.changeScene("mainMenu.fxml");
+        }
+        
         
     }
     
--- tic_tac_toe/src/ticTac/SignUpController.java
+++ tic_tac_toe/src/ticTac/SignUpController.java
@@ -3,22 +3,30 @@ package ticTac;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.regex.Pattern;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 /**
  *
  * @author Moutaz
  */
 public class SignUpController implements Initializable {
-    
+    @FXML
     private TextField username;
+    @FXML
     private TextField password;
+    @FXML
     private Label usernameValid;
+    @FXML
     private Label passwordValid;
+    @FXML
+    private Label passwdAndUser;
     
     @FXML
     private void changeToLoginScreen(ActionEvent event) throws IOException {
@@ -31,11 +39,34 @@ public class SignUpController implements Initializable {
        to Login screen */
     @FXML
     private void SignUpButtonAction(ActionEvent event) throws IOException {
-        
-        MainScreen mainScreen = new MainScreen();
-        mainScreen.changeScene("mainMenu.fxml");
+        //Make sure the user doesn't leave the username or password empty
+        if(username.getText().isEmpty() || password.getText().isEmpty()){    
+            passwdAndUser.setVisible(true);
+            //Once he/she starts writing, invalid label disappears
+            username.addEventHandler(KeyEvent.KEY_TYPED, new EventHandler<KeyEvent>(){
+                @Override
+                public void handle(KeyEvent event) {
+                    passwdAndUser.setVisible(false);
+                }
+        });
+            //Same with password
+            password.addEventHandler(KeyEvent.KEY_TYPED, new EventHandler<KeyEvent>(){
+                @Override
+                public void handle(KeyEvent event) {
+                    passwdAndUser.setVisible(false);
+                }
+                
+            });
+        }else{
+            MainScreen mainScreen = new MainScreen();
+            mainScreen.changeScene("mainMenu.fxml");
+        }
         
     }
+    
+    
+    
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
