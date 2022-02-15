970bc5cda24c609fb0cbf084a0b3249fbf885330
Ahmed-Aladdin <a7medalaa36@gmail.com>
2022-02-15 22:02:49 +0200

    player class

---
+++ tic_tac_toe/src/ticTac/Player.java
@@ -0,0 +1,32 @@
+package ticTac;
+
+public class Player {
+    private int score;
+    private String status;
+    private String username;
+    private String password;
+
+
+    public Player(String username ,String password ,int score)
+    {
+        this.username = username;
+        this.password = password;
+        this.score = score;
+    }
+
+    public int getScore() {return score;}
+
+    public void setScore(int score) {this.score = score;}
+
+    public String getStatus() {return status;}
+
+    public void setStatus(String status) {this.status = status;}
+
+    public String getUsername() {return username;}
+
+    public void setUsername(String username) {this.username = username;}
+
+    public String getPassword() {return password;}
+
+    public void setPassword(String password) {this.password = password;}
+}
